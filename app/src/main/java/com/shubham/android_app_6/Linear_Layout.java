b7a4045f199bdc6c4378a3d8b63d2d20c4277cae
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-01-09 22:38:59 +0530

    update

---
+++ app/src/main/java/com/shubham/android_app_6/Linear_Layout.java
@@ -0,0 +1,14 @@
+package com.shubham.android_app_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Linear_Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
