753b7a721b1c692ebb22362bd75e107daf087d5d
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-01-10 01:52:35 +0530

    update

---
+++ app/src/main/java/com/shubham/android_app_6/Constraint_Layout.java
@@ -0,0 +1,14 @@
+package com.shubham.android_app_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Constraint_Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
---
+++ app/src/main/java/com/shubham/android_app_6/Relative_Layout.java
@@ -0,0 +1,14 @@
+package com.shubham.android_app_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Relative_Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
